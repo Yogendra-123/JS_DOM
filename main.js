a7be63f722f56d5eb5b6684a22c871ae3c020c98
= <=>
2022-12-13 10:18:28 +0530

    JS_DOM

---
+++ main.js
@@ -0,0 +1,29 @@
+// Q.1 
+let myText = document.getElementById('my_text')
+console.log(myText);
+
+// Q.2
+let myHeading = document.getElementsByTagName('h1')
+console.log(myHeading);
+// Q.3
+let myBox = document.getElementsByClassName('box')
+console.log(myBox);
+
+// Q.4
+let changeContent = document.querySelector('#hello_text').textContent = 'Hello World'
+console.log(changeContent);
+
+
+// Q.5
+document.querySelector('button').addEventListener('click', (e) =>{
+    e.preventDefault()
+})
+function myButtonClick(){
+    
+    var myFlex = document.querySelector('#flex-container')
+    if(myFlex.style.flexDirection == 'row'){
+        myFlex.style.flexDirection = 'column'
+    }else{
+        myFlex.style.flexDirection = 'row'
+    }
+}
